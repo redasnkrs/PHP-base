1b2cf305bfa3696a5702c40cc88c339dc748fad3
redasnkrs <redadahmani34@gmail.com>
2025-01-14 15:58:17 +0100

    exe'ss-10 Reda.

---
+++ stagiaires/reda/10-exe-array-assoc.php
@@ -0,0 +1,10 @@
+<?php
+$monTabAssoc =  [
+  "prenom" => "Michaël",
+  "nom" => "Pitz",
+  "caracteristique" => "égocentrique",
+  "dateDeNaissance" => "1951-03-02"
+];
+foreach ($monTabAssoc as $x => $y) {
+  echo "$x = $y <hr>";
+};
