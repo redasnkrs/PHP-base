8ed3e5f51c21a64d14eeec2db3e9774a65831380
Formateurs Web <146428844+WebDevCF2m@users.noreply.github.com>
2025-02-06 13:24:04 +0100

    Update 20-ma-fonction.php

--- stagiaires/Soulaiman/20-ma-fonction.php
+++ stagiaires/Soulaiman/20-ma-fonction.php
@@ -2,9 +2,9 @@
 
 function estPair($nombre) {
         if ($nombre % 2 === 0) {
-            return  "Le nombre ${nombre} est pair ";
+            return  "Le nombre $nombre est pair ";
         } else {
-            return " Le nombre ${nombre} est impair";
+            return " Le nombre $nombre est impair";
         }
 }
 echo estPair(3). "<hr>"; 
