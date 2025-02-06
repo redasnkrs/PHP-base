5ecc849aac09993d780b825bbaef050356e1a786
WebDevCF2m <gitweb@cf2m.be>
2025-02-06 13:43:31 +0100

    connectDB

---
+++ stagiaires/michael/connectDB/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// constantes de la base de donnée
+const DB_MYSQL_TYPE = "mysql";
+const DB_MYSQL_HOST = "localhost"; // hôte de connexion (url ou adresse ip)
+const DB_MYSQL_NAME = "agim_connect"; // nom de la DB
+const DB_MYSQL_LOGIN = "root"; // login
+const DB_MYSQL_PWD = ""; // mot de passe
+const DB_MYSQL_PORT = 3306;
+const DB_MYSQL_ENCODE = "utf8";
