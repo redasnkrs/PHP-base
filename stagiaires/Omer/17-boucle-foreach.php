9133c4e63ba5260d21dfadc78a3907f43c71211d
OmerAslani <aslaniomer21@gmail.com>
2025-02-09 11:25:46 +0100

    piere folders added

--- stagiaires/Omer/17-boucle-foreach.php
+++ stagiaires/Omer/17-boucle-foreach.php
@@ -23,7 +23,7 @@
     ?>
     <br>
     <hr>
-    <h1>Affichez les lettres de Stab avec une boucle foreach en affichant aussi la clef de chaque élément</h1>
+    <h1>2. Affichez les lettres de Stab avec une boucle foreach en affichant aussi la clef de chaque élément</h1>
     <?php
     $i = 0;
     foreach ($abcd as $abcde) {
@@ -35,7 +35,7 @@
 
     <br>
     <hr>
-    <h1>Affichez les jours de la semaine avec une boucle foreach en affichant aussi la clef de chaque élément</h1>
+    <h1>3. Affichez les jours de la semaine avec une boucle foreach en affichant aussi la clef de chaque élément</h1>
     <?php
     $i = 0;
     foreach ($days as $day) {
@@ -48,8 +48,7 @@
 
     <br>
     <hr>
-    <h1>Affichez le jour actuel de la semaine en français avec la fonction date() et la variable SsemaineFr
-        jeudi</h1>
+    <h1>4. Affichez le jour actuel de la semaine en français avec la fonction date() et la variable SsemaineFr</h1>
 
     <?php
     foreach ($days as $day) {
@@ -60,6 +59,44 @@
         }
     }
 
+    ?>
+
+    <br>
+    <hr>
+    <h1>5. Affichez la liste des prénoms du tableau $classe avec une boucle foreach</h1>
+    <?php
+
+    foreach ($web1 as $prenom) {
+        echo $prenom["prenom"] . " ";
+
+    }
+
+    ?>
+
+    <br>
+    <hr>
+    <h1>6. Affichez la liste des 'nom', 'age' et 'note' du tableau $classe18 avec une boucle foreach avec un retour à la
+        ligne entre chaque élément</h1>
+    <?php
+
+    foreach ($web1 as $eleve) {
+        echo "Name: " . $eleve["nom"] . " | Age: " . $eleve["age"] . " | Note: " . $eleve["note"] . "<br>";
+
+    }
+
+
+    ?>
+
+    <br>
+    <hr>
+    <h1>7. Affichez la liste des 'nom', 'age' et 'note' du tableau $classe18 avec une boucle foreach en affichant aussi
+        la clef de chaque élément</h1>
+    <?php
+
+    foreach ($countryCode as $country) {
+        echo "<br>";
+        echo   $country[0] . " |  " . $country[1] . " |  " . $country[2] . " |  " . $country[3] . " | " . "<br>";
+    }
     ?>
 
 </body>
