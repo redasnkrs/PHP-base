9b59134a196ea9cc7ef97367ab8c0b8773ccdd2c
WebDevCF2m <gitweb@cf2m.be>
2025-02-10 14:04:02 +0100

    17

--- formateur/17-boucle-foreach.php
+++ formateur/17-boucle-foreach.php
@@ -11,6 +11,10 @@ $tab = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j");
 
 echo '<p>1) Affichez les lettres de $tab avec une boucle foreach<br>';
 
+foreach($tab as $value){
+    echo $value." ";
+}
+
 
 
 echo "</p>";
@@ -22,7 +26,10 @@ echo "</p>";
 
 echo '<p>2) Affichez les lettres de $tab avec une boucle foreach en affichant aussi la clef de chaque élément<br>';
 
-
+foreach($tab as $clef => $valeur){
+    // ! la virgule ne passe qu'avec echo (pas print ni attribution de variables), préférer le "."
+    echo $clef," => ",$valeur," | ";
+}
 
 echo "</p>";
 
@@ -34,7 +41,9 @@ echo "</p>";
 
 echo '<p>3) Affichez les jours de la semaine avec une boucle foreach en affichant aussi la clef de chaque élément<br>';
 
-
+foreach($semaineFr as $k => $v):
+    echo "$k => $v | ";
+endforeach;
 
 echo "</p>";
 
@@ -43,6 +52,12 @@ echo "</p>";
  */
 
 echo '<p>4) Affichez le jour actuel de la semaine en français avec la fonction date() et la variable $semaineFr<br>';
+// var_dump($semaineFr);
+// https://www.php.net/manual/fr/function.date.php
+// https://www.php.net/manual/fr/datetime.format.php
+
+$jour = $semaineFr[date("N")];
+echo $jour;
 
 echo "</p>";
 
@@ -52,7 +67,9 @@ echo "</p>";
 
 echo '<p>5) Affichez la liste des prénoms du tableau $classe avec une boucle foreach<br>';
 
-
+foreach($classe as $classe2){
+    echo "$classe2 ";
+}
 
 echo "</p>";
 
@@ -63,7 +80,10 @@ echo "</p>";
 
 echo '<p>6) Affichez la liste des \'nom\', \'age\' et \'note\' du tableau $classe18 avec une boucle foreach avec un retour à la ligne entre chaque élément<br>';
 
-
+// PHP 'triche' en acceptant les clef de type string sans les guillemets (à éviter), si on souhaite ne mettre les guillemets, les brackets (accolades) protègent la variable (à privilégier)
+foreach($classe18 as $x){
+    echo "nom : ".$x['nom']." | âge : {$x['age']} | note : $x[note] <br>";
+}
 
 echo "</p>";
 
@@ -73,7 +93,12 @@ echo "</p>";
 
 echo '<p>7) Avec 2 boucles foreach, affichez tous les champs la liste des pays du tableau $countryCode<br>';
 
-
+foreach($countryCode as $country){
+    foreach($country as $values){
+        echo "$values | ";
+    }
+    echo "<br>";
+}
 
 
 echo "</p>";
