d52065f81915e1778626cac05c0538251a6088eb
Misikalenga <misikalenga@gmail.com>
2025-02-10 14:45:29 +0100

    php exe19

---
+++ stagiaires/misi/19-recap-CF/view/inc/navView.php
@@ -0,0 +1,10 @@
+<a href="./"><img src="images/logo-cf.png" class="logo"></a>
+		<div>
+			<ul id="topmenu">
+				<li><a href="./">Accueil</a></li>
+				<li><a href="./?page=formations">Nos formations</a></li>
+				<li><a href="./?page=programme">Notre programme</a></li>
+				<li><a href="./?page=newsletter">Newsletter</a></li>
+				<li><a href="./?page=contact">Contact</a></li>		
+			</ul>
+		</div>
