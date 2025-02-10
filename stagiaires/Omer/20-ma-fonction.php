2f47f5407b141412441f108c44408b4baa26b4ec
OkiSenpai <omersatelit@gmail.com>
2025-02-10 10:54:46 +0100

    exe 20 added and readme changed

---
+++ stagiaires/Omer/20-ma-fonction.php
@@ -0,0 +1,15 @@
+<?php
+
+// Crée un fichier nommé 20-ma-fonction.php dans laquelle tu crée une fonction nommée estPair qui vérifie si un entier est pair, puis dans le même fichier testez la fonction !
+
+function estPair()
+{
+    $randomNum = rand(1, 100);
+    if ($randomNum % 2 === 0) {
+        echo $randomNum . " est pair";
+    } else {
+        echo $randomNum . " est inpair";
+    }
+}
+
+estPair();
