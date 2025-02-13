b3f3157d6df197869d5009e573c966551a662897
Géraldine Ungaro <ungaro.geraldine@gmail.com>
2025-02-13 15:29:35 +0100

    exe19

--- stagiaires/geraldineungaro/19-recap-CF/public/index.php
+++ stagiaires/geraldineungaro/19-recap-CF/public/index.php
@@ -9,6 +9,28 @@
 
 // Gestion des variables GET
 
+if (isset($GET['p'])){
+switch ($GET['p']) {
+    case 'formations':
+        include "../view/formationView.php";
+        break;
+    case 'formulaire':
+        include "../view/formulaireView.php";
+        break;
+    case 'newsletter':
+        include "../view/newsletterView.php";
+        break;
+    case 'programme':
+        include "../view/programmeView.php";
+        break;
+    default:
+        include "../view/erreur404.php";
+        break;
+}
+}
+
 
 // Appel d'une des vues (par défaut l'accueil)
 include "../view/homepageView.php";
+
+//var_dump($_GET);
--- stagiaires/geraldineungaro/19-recap-CF/view/formationView.php
+++ stagiaires/geraldineungaro/19-recap-CF/view/formationView.php
@@ -1,155 +1,18 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Apprendre le développement Web | Accueil</title>
+	<title>Apprendre le développement Web | Formations</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://kit.fontawesome.com/a62a90acc1.js" crossorigin="anonymous"></script>
 </head>
 <body>
 	<section class="nav-bar">
-	<?php
-	include ("inc/navView.php");
-	?>
-	</section>
 
-<!--Section header -->
-	<section class="header">
-		<h1>Apprendre le développement Web</h1>
-		<h3>Découvrir nos formations</h3>
-		<a href="formations.html" class="btn">Nos formations</a>
-	</section>
-
-
-<!--Section de formations -->
-	<section class="formations">
-		<h1>Nos formations récentes</h1>
-		<p>Lorem Ipsum est simplement du faux texte employé dans la composition</p>
-
-		<div class="row">
-			
-			<div class="formations-col">
-				<img src="images/html-css.jpg">
-				<div class="prix">199.99€</div>
-				<h3>HTML & CSS</h3>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-
-				<div class="star-formation">
-					<i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-			        <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <span>(3454)</span>
-				</div>
-			
-
-				<a href="formations.html" class="btn-formation">Découvrir la formation</a>
-			</div>
-
-
-			<div class="formations-col">
-				<img src="images/php.jpg">
-				<div class="prix">199.99€</div>
-				<h3>PHP</h3>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-
-				<div class="star-formation">
-					<i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-			        <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <span>(6655)</span>
-				</div>
-				
-				<a href="formations.html" class="btn-formation">Découvrir la formation</a>
-			</div>
-
-			<div class="formations-col">
-				<img src="images/javascript.jpg">
-				<div class="prix">199.99€</div>
-				<h3>JavaScript</h3>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-
-				<div class="star-formation">
-					<i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-			        <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <span>(87)</span>
-				</div>
-
-				<a href="formations.html" class="btn-formation">Découvrir la formation</a>
-			</div>
-
-		</div>
-
-
-		<div class="row">
-			
-			<div class="formations-col">
-				<img src="images/data-science.jpg">
-				<div class="prix">199.99€</div>
-				<h3>Data Science</h3>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-
-				<div class="star-formation">
-					<i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-			        <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <span>(777)</span>
-				</div>
-
-				<a href="formations.html" class="btn-formation">Découvrir la formation</a>
-			</div>
-
-
-			<div class="formations-col">
-				<img src="images/php-poo.jpg">
-				<div class="prix">199.99€</div>
-				<h3>PHP Orienté Objet</h3>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-
-				<div class="star-formation">
-					<i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-			        <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <i class="fa-regular fa-star"></i>
-				    <span>(65)</span>
-				</div>
-
-				<a href="formations.html" class="btn-formation">Découvrir la formation</a>
-			</div>
-
-			<div class="formations-col">
-				<img src="images/mvc.jpg">
-				<div class="prix">199.99€</div>
-				<h3>MVC</h3>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</p>
-
-				<div class="star-formation">
-					<i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-			        <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star"></i>
-				    <i class="fa-solid fa-star-half"></i>
-				    <span>(9999)</span>
-				</div>
-
-				<a href="formations.html" class="btn-formation">Découvrir la formation</a>
-			</div>
-
-		</div>
+	
 
 	</section>
-
-<!-- ---------------Section A propos ------------------>
 	<section class="apropos">
 		<div>
 			<h1>Apropos de nous</h1>
@@ -314,10 +177,68 @@
 
 <!-- Section footer------------------->
 <footer>
-<?php
-	include ("inc/footerView.php");
-	?>
+	
+	<div class="colonne">
+		<h3>Informations de contact</h3>
+		<div class="contact-infos-detail">
+			<h4>Adresse</h4>
+			<div>
+				<i class="fa-sharp fa-solid fa-location-dot"></i>
+				<p>565, rue Victor Hugo, 75000 Paris</p>
+			</div>
+		</div>
+		<div class="contact-infos-detail">
+			<h4>Téléphone</h4>
+			<div>
+				<i class="fa-solid fa-phone"></i>
+				<p>09797797977</p>
+			</div>
+		</div>
+		<div class="contact-infos-detail">
+			<h4>Adresse Email</h4>
+			<div>
+				<i class="fa-solid fa-envelope"></i>
+				<p>skilldeve dot com</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="colonne">
+		<h3>À propos</h3>
+		<a href="#">À propos de nous</a>
+		<a href="#">Contact</a>
+		<a href="#">Politique de confidentialité</a>
+		<a href="#">Conditions générales</a>
+	</div>
+
+	<div class="colonne">
+		<h3>Catégories de formations</h3>
+		<a href="#">Développement Web</a>
+		<a href="#">Développement Logiciels</a>
+		<a href="#">Data Analyse</a>
+		<a href="#">Data Science</a>
+	</div>
+
+
+	<div class="copyright">
+		<p>© Copyright - Skilldev 2023</p>
+	</div>
+
+
+
+
 </footer>
+
+
+
+
+
+
+
+
+
+
+
 
 
 </body>
