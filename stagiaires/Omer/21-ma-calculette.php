b0234e512d97a352b3582ac9864dee967f20524d
OmerAslani <aslaniomer21@gmail.com>
2025-02-15 16:01:19 +0100

    exe 21

--- stagiaires/Omer/21-ma-calculette.php
+++ stagiaires/Omer/21-ma-calculette.php
@@ -1,22 +1,4 @@
 <?php
-
-// Crée un fichier nommé 20-ma-fonction.php dans laquelle tu crée une fonction nommée estPair qui vérifie si un entier est pair, puis dans le même fichier testez la fonction !
-
-function estPair()
-{
-    $randomNum = rand(1, 100);
-    if ($randomNum % 2 === 0) {
-        echo $randomNum . " est pair";
-    } else {
-        echo $randomNum . " est inpair";
-    }
-}
-
-estPair();
-
-
-
-<?php
 function calculSimple($nb1,$nb2,$opertator){
    
     if($opertator == '+'){
